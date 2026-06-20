5dce671b4a25e5046786b0f9c02512a3284ccf3e
Jack White <ataev0461@gmail.com>
2026-06-20 11:40:33 +0300

    TASK-1. Создать скелет приложения

---
+++ src/main/java/org/example/msaccountreservation/MsAccountReservationApplication.java
@@ -0,0 +1,13 @@
+package org.example.msaccountreservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsAccountReservationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsAccountReservationApplication.class, args);
+    }
+
+}
