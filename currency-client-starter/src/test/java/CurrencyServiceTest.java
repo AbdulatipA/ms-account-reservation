26ba715173b23b2575cf8b7d9d7fff99c972449a
Jack White <ataev0461@gmail.com>
2026-07-26 16:57:13 +0300

    TASK-9. Реализовать кэширование курса валют с помощью Redis

--- currency-client-starter/src/test/java/CurrencyServiceTest.java
+++ currency-client-starter/src/test/java/CurrencyServiceTest.java
@@ -29,7 +29,7 @@ import java.util.Map;
  @Mock
  private Counter counter;
 
-
+//
  @BeforeEach
  void setUp() {
   currencyService = new CurrencyService("my-api-key", currencyClient, meterRegistry);
